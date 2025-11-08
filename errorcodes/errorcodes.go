a1f53db96fb8e399bccee5a3426f53b7fc42ab79
anchal-dezerv <anchal.satishkumar@dezerv.in>
2025-11-08 23:30:53 +0530

    basic structure done

--- errorcodes/errorcodes.go
+++ errorcodes/errorcodes.go
@@ -1,1 +1,29 @@
 package errorcodes
+
+import "net/http"
+
+type ErrorCode uint
+
+const (
+	NoError             ErrorCode = 0
+	InternalServerError ErrorCode = 01
+)
+
+func (e ErrorCode) Message() string {
+	messages := map[ErrorCode]string{
+		NoError:             "",
+		InternalServerError: "internal server error",
+	}
+	m := messages[e]
+	return m
+}
+
+func (e ErrorCode) HttpStatusCode() int {
+	httpErrorStatus := map[ErrorCode]int{
+		NoError:             http.StatusOK,
+		InternalServerError: http.StatusInternalServerError,
+	}
+
+	hes := httpErrorStatus[e]
+	return hes
+}
--- services.go/token.go
+++ services.go/token.go
@@ -1,16 +1,28 @@
 package services
 
 import (
+	"log"
+	"task_manager/errorcodes"
 	"task_manager/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
+type TokenHandlerResponse struct {
+	Success      bool                 `json:"success"`
+	ErrorCode    errorcodes.ErrorCode `json:"errorcode"`
+	ErrorMessage string               `json:"error"`
+	Data         string               `json:"data,omitempty"`
+}
+
 func TokenHandler(c *gin.Context) {
+	error := errorcodes.NoError
 	tokenString, err := middlewares.GenerateToken("user123")
 	if err != nil {
-		c.JSON(500, gin.H{"error": "could not sign token"})
+		log.Println(`error: could not sign token`)
+		error = errorcodes.InternalServerError
+		c.JSON(error.HttpStatusCode(), TokenHandlerResponse{Success: false, ErrorCode: error, ErrorMessage: error.Message()})
 		return
 	}
-	c.JSON(200, gin.H{"token": tokenString})
+	c.JSON(error.HttpStatusCode(), TokenHandlerResponse{Success: true, ErrorCode: error, ErrorMessage: error.Message(), Data: tokenString})
 }
