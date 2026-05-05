606821f750d54929a9184aec27c5129130a1f9c9
anchalm03 <malanianchal123@gmail.com>
2026-05-06 02:13:21 +0530

    CORs change

--- middlewares/allowCORS.go
+++ middlewares/allowCORS.go
@@ -1,1 +1,25 @@
 package middlewares
+
+import (
+	"strings"
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func AllowCORS() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost:") {
+				return true
+			}
+			return strings.HasSuffix(origin, ".preview.emergentagent.com")
+		},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+}
